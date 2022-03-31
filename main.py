ed97118144d89839b4d2843e4092acaee9190530
Sania2001 <saniagshinde@gmail.com>
2022-03-31 14:48:12 +0530

    .

---
+++ main.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
+product = input("Enter product name:")
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.maximize_window()
+driver.get("https://www.flipkart.com/")
+time.sleep(1)
+driver.find_element_by_xpath("/html/body/div[2]/div/div/button").click()
+time.sleep(1)
+driver.find_element_by_name("q").send_keys(product)
+time.sleep(1)
+driver.find_element_by_class_name("L0Z3Pu").submit()
+time.sleep(5)
+driver.close()
